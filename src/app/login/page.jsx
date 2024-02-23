76732efa48c30197db7d64623f5229c7ee452487
Selorm <ranselorm@outlook.com>
2024-02-23 22:18:36 +0000

    Add login form

---
+++ src/app/login/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center items-center mt-[60px]">
+      <div>
+        <div>Login with Google</div>
+        <div>Login with GitHub</div>
+        <div>Login with Facebook</div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
