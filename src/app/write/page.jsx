ba8c9a221fbd7ead943a0a731309cf57f59fd880
Selorm <ranselorm@outlook.com>
2024-02-29 20:35:55 +0000

    Add write component

---
+++ src/app/write/page.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+const Write = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Title" />
+      {/* editor */}
+      <div>
+        <Image src="" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Write;
